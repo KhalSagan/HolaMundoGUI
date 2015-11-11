89df66b07e4fa0efa0604ef33f421bd7ba85ed09
a044519180n <a044519180n@s09-pc13>
2015-11-11 20:03:33 +0100

    Inicio

---
+++ src/holamundogui/HolaMundoGUI.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundogui;
+
+/**
+ *
+ * @author a044519180n
+ */
+public class HolaMundoGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("ROJO");
+
+    }
+    
+}
